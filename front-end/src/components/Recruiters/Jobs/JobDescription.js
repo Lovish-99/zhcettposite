62eeab35ae19af617eadc81733e013591e5e4074
BHUMIKA SHARMA <bhumisha2@gmail.com>
2023-04-26 17:01:35 +0530

    NEW

--- front-end/src/components/Recruiters/Jobs/JobDescription.js
+++ front-end/src/components/Recruiters/Jobs/JobDescription.js
@@ -43,6 +43,35 @@ const JobDescription = () => {
     }
   };
 
+  function openBase64NewTab(base64Pdf) {
+    var blob = base64toBlob(base64Pdf);
+    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveOrOpenBlob(blob, "pdfBase64.pdf");
+    } else {
+      const blobUrl = URL.createObjectURL(blob);
+      window.open(blobUrl);
+    }
+  }
+
+  function base64toBlob(base64Data) {
+    const sliceSize = 1024;
+    const byteCharacters = atob(base64Data);
+    const bytesLength = byteCharacters.length;
+    const slicesCount = Math.ceil(bytesLength / sliceSize);
+    const byteArrays = new Array(slicesCount);
+
+    for (let sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+      const begin = sliceIndex * sliceSize;
+      const end = Math.min(begin + sliceSize, bytesLength);
+
+      const bytes = new Array(end - begin);
+      for (let offset = begin, i = 0; offset < end; ++i, ++offset) {
+        bytes[i] = byteCharacters[offset].charCodeAt(0);
+      }
+      byteArrays[sliceIndex] = new Uint8Array(bytes);
+    }
+    return new Blob(byteArrays, { type: "application/pdf" });
+  }
   return (
     <MDBContainer>
       <MDBCard className="text-black m-5" style={{ borderRadius: "0px" }}>
